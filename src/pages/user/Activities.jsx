2966278e466f80482a2626f5c83dce9ed888bd7d
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-07-10 04:25:58 +0530

    [ADD] Activities Page UI added

---
+++ src/pages/user/Activities.jsx
@@ -0,0 +1,78 @@
+import { Tabs, TabList, Tab, Box, Flex } from "@chakra-ui/react";
+import React, { useEffect, useState } from 'react';
+import { useColorMode } from "@chakra-ui/react";
+import {colors} from "../../constants/contants"
+import EnquiriesReceived from "../../components/Activities/EnquiriesReceived"
+import EnquiriesSent from "../../components/Activities/EnquiriesSent"
+import OrdersReceived from "../../components/Activities/OrdersRecieved"
+import OrdersPlaced from "../../components/Activities/OrdersSent"
+import VirtualPlayDates from "../../components/Activities/VpdRequests"
+import Sidebar from "../../components/SideBar";
+import { toast } from 'react-toastify';
+import axios from "axios";
+import { baseUrl } from "../../constants/contants";
+import { authStatus } from "../../recoilAtoms/Auth";
+import { useRecoilState } from "recoil";
+import {fetchUserById} from "../../api/users";
+
+function Activities() {
+  const tabs = [
+    { label: "Enquiries Received", component: EnquiriesReceived },
+    { label: "Enquiries Sent", component: EnquiriesSent },
+    { label: "Orders Received", component: OrdersReceived },
+    { label: "Orders Placed", component: OrdersPlaced },
+    { label: "Virtual Play Dates", component: VirtualPlayDates },
+  ];
+
+  const [selectedTab, setSelectedTab] = useState(0);
+  const SelectedComponent = tabs[selectedTab].component;
+  const [data, setData] = useState([]);
+  const [authStatuss, setAuthStatus] = useRecoilState(authStatus);
+
+
+  async function fetchData() {
+    try {
+    
+      console.log("hello"+authStatuss.userName);
+      const res= await fetchUserById(authStatuss.userName);
+      console.log(res.data);
+      setData(res.data);
+    } catch (error) {
+      toast("🤐 An unknown error occurred... Try again!");
+      console.log("An error occurred while making the request:", error);
+    }
+  }
+  useEffect(()=>{
+    fetchData();
+  },[]);
+  return (
+    <Flex>
+      <Sidebar></Sidebar>
+      <Box width={"100%"} >
+        <Tabs mt={5}
+          variant="enclosed"
+          colorScheme="brand"
+          size="md"
+          onChange={(index) => setSelectedTab(index)}
+        >
+          <TabList display="flex" justifyContent="space-around" textAlign={'center'}>
+            {tabs.map((tab, index) => (
+              <Tab
+                key={index}
+                _selected={{ color: "black", bg: "brand.500" }}
+                _hover={{ bg: "brand.200" }}
+              >
+                {tab.label}
+              </Tab>
+            ))}
+          </TabList>
+        </Tabs>
+        <Box mt={4}>
+          <SelectedComponent data={data} />
+        </Box>
+      </Box>
+    </Flex>
+    
+  );
+}
+export default Activities;
